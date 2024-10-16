24013033b72f6d80f40dfaa466b3641ab81cde3b
DianaTsebenko <cebenkodiana@gmail.com>
2024-10-16 20:15:51 +0300

    Finish task 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
 
-import Profile from './Profile/Profile';
-import user from './Profile/user.json';
-
-import Statistics from './Statistics/Statistics';
-import data from './Statistics/data.json';
-
-import FriendList from './FriendsList/FriendList';
-import friends from './FriendsList/friends.json';
-
-import TransactionHistory from './Transaction/TransactionHistory';
-import transactions from './Transaction/transactions.json';
+import FeedbackWidget from './Feedback/Feedback';
 
 const App = () => {
   return (
     <div>
-      <h1>Task 1</h1>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <h1>Task 2</h1>
-      <Statistics title="Upload stats" stats={data} />
-      <h1>Task 3</h1>
-      <FriendList friends={friends} />
-      <h1>Task 4</h1>
-      <TransactionHistory transactions={transactions} />
+      <FeedbackWidget />
     </div>
   );
 };
