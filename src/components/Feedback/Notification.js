d036677e30fd9b6fa6a39db720b365667dcbc1c4
DianaTsebenko <95097790+DianaTsebenko@users.noreply.github.com>
2024-10-16 20:25:28 +0300

    Delete src directory

--- src/components/Feedback/Notification.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Notification = ({ message }) => (
-  <div>
-    <p>{message}</p>
-  </div>
-);
-
-export default Notification;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import FeedbackWidget from 'components/Feedback/Feedback';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FeedbackWidget />
-  </React.StrictMode>
-);
